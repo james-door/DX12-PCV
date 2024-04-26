7888cf4e3023c882e23ea6645c34518d8f1f9cd6
james-door <103546187+james-door@users.noreply.github.com>
2024-04-26 15:34:57 +1000

    cleanup shader

--- shaders/fragmentShader.hlsl
+++ shaders/fragmentShader.hlsl
@@ -4,9 +4,7 @@ struct InputAttributes
     float3 colour : COLOR;
 };
 
-//[earlydepthstencil]
 float4 main(InputAttributes IN) : SV_Target0
 {
-    //depth = IN.clipPos.z / IN.clipPos.w * 0.5 + 0.5; // This attempts to modify the depth value.
     return float4(IN.colour, 1.0f);
 }
